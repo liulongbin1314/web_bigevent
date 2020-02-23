c17fa64dcf7afca5b9b018352afafc4b139c3581
bin <liulongbin1314@outlook.com>
2020-02-23 18:11:25 +0800

    完成了主页功能的开发

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -4,4 +4,24 @@
 $.ajaxPrefilter(function(options) {
   // 在发起真正的 Ajax 请求之前，统一拼接请求的根路径
   options.url = 'http://ajax.frontend.itheima.net' + options.url
+
+  // 统一为有权限的接口，设置 headers 请求头
+  if (options.url.indexOf('/my/') !== -1) {
+    options.headers = {
+      Authorization: localStorage.getItem('token') || ''
+    }
+  }
+
+  // 全局统一挂载 complete 回调函数
+  options.complete = function(res) {
+    // console.log('执行了 complete 回调：')
+    // console.log(res)
+    // 在 complete 回调函数中，可以使用 res.responseJSON 拿到服务器响应回来的数据
+    if (res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败！') {
+      // 1. 强制清空 token
+      localStorage.removeItem('token')
+      // 2. 强制跳转到登录页面
+      location.href = '/login.html'
+    }
+  }
 })
