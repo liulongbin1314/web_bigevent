121eb66386fd9db92ef645d2928f3ec8f7a7aaf4
bin <liulongbin1314@outlook.com>
2020-03-20 17:19:33 +0800

    完成文章管理相关功能的开发

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -3,7 +3,8 @@
 // 在这个函数中，可以拿到我们给Ajax提供的配置对象
 $.ajaxPrefilter(function(options) {
   // 在发起真正的 Ajax 请求之前，统一拼接请求的根路径
-  options.url = 'http://ajax.frontend.itheima.net' + options.url
+  // options.url = 'http://ajax.frontend.itheima.net' + options.url
+  options.url = 'http://www.liulongbin.top:3007' + options.url
 
   // 统一为有权限的接口，设置 headers 请求头
   if (options.url.indexOf('/my/') !== -1) {
